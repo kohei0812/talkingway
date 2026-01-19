3f485a35f059babef63584eab3c494003aa95f21
kohei <kouhei.stifler@gmail.com>
2026-01-19 12:25:42 +0900

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -49,7 +49,7 @@ export default function AboutPage() {
             <Link href="https://docs.google.com/spreadsheets/d/1KT7hkj4io88d0Vp0ia2jpeye5Y6g-OhyzJzZtr4S7pc/edit" target="_blank">
               【FF14】有料対話店一覧
             </Link>
-            の転載となります。
+            の転載（自動連携）にて表示しています。
           </li>
           <li>営業中判定を含む掲載内容は実際と異なる場合があります。最新の情報は各公式情報をご確認ください。</li>
           <li>
